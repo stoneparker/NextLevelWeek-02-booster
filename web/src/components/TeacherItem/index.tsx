9c3529c1c42c7b2416b6cdd2d87c81847dd1b6d9
unknown <heyvitoria.lopes@gmail.com>
2020-08-08 18:58:41 -0300

    finalização do frontend web

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,33 +3,52 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-const TeacherItem: React.FC = () => {
+export interface Teacher {
+   avatar: string;
+   bio: string;
+   cost: number;
+   id: number;
+   name: string;
+   subject: string;
+   whatsapp: string;
+}
+
+interface TeacherItemProps {
+   teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+   function createNewConnection() {
+      api.post('connections', { user_id: teacher.id })
+   }
+
   return (
    <article className="teacher-item">
       <header>
-         <img src="https://pbs.twimg.com/profile_images/1290374806353907716/Mz5DqTxe_400x400.jpg" alt="Vitória Lopes"/>
+         <img src={teacher.avatar} alt={teacher.name} />
          <div>
-            <strong>Vitória Lopes</strong>
-            <span>Física</span>
+            <strong>{teacher.name}</strong>
+            <span>{teacher.subject}</span>
          </div>
       </header>
 
-      <p>
-         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-         <br/><br/>
-         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
          <p>
             Preço/hora
-            <strong>R$ 60,00</strong>
+            <strong>R$ {teacher.cost}</strong>
          </p>
-         <button type="button">
+         <a 
+            target="_blank" 
+            onClick={createNewConnection} 
+            href={`https://wa.me/${teacher.whatsapp}`}
+         >
             <img src={whatsappIcon} alt="Whatsapp"/>
             Entrar em contato
-         </button>
+         </a>
       </footer>
    </article>
   );
